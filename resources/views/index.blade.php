0dc5920de2de876f79c7107e5ab3a426018bf002
Arowosegbe Ifeoluwa <iifeoluwa.ao@gmail.com>
2017-02-10 12:06:12 +0100

    Initial Commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Staff Monitor</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div id="hero">
+		<h4>Staff Monitor</h4>
+	</div>
+</body>
+</html>
